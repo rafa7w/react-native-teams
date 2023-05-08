ef6259b8c94a2d55059a15c772ae3bd824518049
Rafael William <rafael.furlan@inlog.com.br>
2023-05-08 13:56:44 -0300

    custom fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
+import { ActivityIndicator } from 'react-native';
 import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import theme from './src/theme'
 
 export default function App() {
+  // Fazemos o carregamento das fontes aqui para que esteja disponível em toda a aplicação
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoaded ? <Groups /> : <ActivityIndicator />} 
     </ThemeProvider>
   );
 }
