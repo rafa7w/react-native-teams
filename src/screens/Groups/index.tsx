ed40325b07cddeefabba6c79c19654f325ba8208
Rafael William <rafael.fim@hotmail.com>
2023-05-08 20:24:14 -0300

    app navigation

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
@@ -13,6 +14,12 @@ import { Container } from './styles'
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos']);
 
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('new')
+  }
+ 
   return (
     <Container>
       <Header />
@@ -39,6 +46,7 @@ export function Groups() {
 
       <Button 
         title='Criar nova turma'
+        onPress={handleNewGroup}
       />
       
     </Container>
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import { Header } from '@components/Header';
 import { Container, Content , Icon} from './styles';
 import { Highlight } from '@components/Highlight';
@@ -5,6 +6,12 @@ import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 
 export function NewGroup() {
+  const navigation = useNavigation()
+
+  function handleNew() {
+    navigation.navigate('players', {'group': 'Rocket'})
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -15,7 +22,11 @@ export function NewGroup() {
           subtitle='crie a turma para adicionar as pessoas'
         />
         <Input placeholder='Nome da turma'/>
-        <Button title='Criar' style={{marginTop: 20}}/>
+        <Button 
+          title='Criar' 
+          style={{marginTop: 20}}
+          onPress={handleNew}
+        />
       </Content>
     </Container>
   )
