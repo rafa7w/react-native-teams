7c60e54463ddf62d29ae4339e21973190387f733
Rafael William <rafael.fim@hotmail.com>
2023-05-08 19:46:30 -0300

    player card component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,10 +1,15 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
+import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+
 import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
 
 export function Players() {
@@ -42,7 +47,29 @@ export function Players() {
           {players.length}
         </NumbersOfPlayers>
       </HeaderList>
-
+      
+      <FlatList 
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <PlayerCard 
+            name={item}
+            onRemove={() => {}}
+          />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message='Não há pessoas nesse time.'/>
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          {paddingBottom: 100},
+          players.length === 0 && {flex: 1}
+        ]}
+      />
+      <Button 
+        title='Remover turma'
+        type='SECONDARY'
+      />
     </Container>
   )
 }
