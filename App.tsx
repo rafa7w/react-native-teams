4adfdd47c5ae0f811cb656295b5d55194f8b87cd
Rafael William <rafael.fim@hotmail.com>
2023-05-08 18:31:08 -0300

    players interface

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 // import { ActivityIndicator } from 'react-native'
 import { StatusBar } from 'react-native';
-import { NewGroup } from '@screens/NewGroup';
+import { Players } from '@screens/Players';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -18,7 +18,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      { fontsLoaded ? <NewGroup /> : <Loading />} 
+      { fontsLoaded ? <Players /> : <Loading />} 
     </ThemeProvider>
   );
 }
