75867911928413ee3c4fcb85f292e55c9480aa34
Rafael William <rafael.fim@hotmail.com>
2023-05-08 18:22:09 -0300

    placeholder style

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,15 @@
 import { TextInputProps } from 'react-native';
+import { useTheme } from 'styled-components/native';
 import { Container } from './styles';
 
 export function Input({...rest}: TextInputProps) {
+  // { COLORS } também dá para usar - modo desestruturado 
+  const theme = useTheme()
+
   return (
-    <Container {...rest}/>
+    <Container 
+      placeholderTextColor={theme.COLORS.GRAY_300}
+      {...rest}
+    />
   )
 }
