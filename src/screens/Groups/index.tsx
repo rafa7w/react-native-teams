83d0faf533f73459417664d5e415b77a6286a529
Rafael William <rafael.furlan@inlog.com.br>
2023-05-08 15:09:45 -0300

    highlight component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,16 @@
 // Outra forma de importar: import * as S from './styles'
 import { Header } from '@components/Header'
+import { Highlight } from '@components/Highlight'
 import { Container } from './styles'
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight 
+        title='Turmas'
+        subtitle='jogue com a sua turma'
+      />
     </Container>
   );
 }
