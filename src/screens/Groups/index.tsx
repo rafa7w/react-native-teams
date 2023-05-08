768c16e8dedf675edda50b35a1911700d31994f1
Rafael William <rafael.furlan@inlog.com.br>
2023-05-08 14:21:52 -0300

    header component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,11 @@
 // Outra forma de importar: import * as S from './styles'
-import { Container, Title } from './styles'
+import { Header } from '@components/Header'
+import { Container } from './styles'
 
 export function Groups() {
   return (
     <Container>
-      <Title>Groups</Title>
+      <Header />
     </Container>
   );
 }
