e7aba227597c2d10e11af64284ec1877c3063e37
Rafael William <rafael.furlan@inlog.com.br>
2023-05-08 14:05:34 -0300

    loading component

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
-import { ActivityIndicator } from 'react-native';
+// import { ActivityIndicator } from 'react-native'
 import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import theme from './src/theme'
+
+import { Loading } from '@components/Loading';
+import theme from './src/theme';
 
 export default function App() {
   // Fazemos o carregamento das fontes aqui para que esteja disponível em toda a aplicação
@@ -10,7 +12,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups /> : <ActivityIndicator />} 
+      { fontsLoaded ? <Groups /> : <Loading />} 
     </ThemeProvider>
   );
 }
