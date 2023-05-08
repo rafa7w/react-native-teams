83f59cf6d449df07c5b37ec2b889bfdc8f740f0b
Rafael William <rafael.fim@hotmail.com>
2023-05-08 19:06:57 -0300

    button icon component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,8 @@
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
-import { Container } from './styles';
+import { ButtonIcon } from '@components/ButtonIcon';
+import { Input } from '@components/Input';
+import { Container, Form } from './styles';
 
 export function Players() {
   return (
@@ -11,6 +13,10 @@ export function Players() {
         title='Nome da turma'
         subtitle='adicione a galera e separe os times'
       />
+      <Form>
+        <Input placeholder='Nome da pessoa' autoCorrect={false}/>
+        <ButtonIcon icon='add'/>
+      </Form> 
     </Container>
   )
 }
