d6ba9032a5762de96b158d999637842dfbc8f759
Rafael William <rafael.furlan@inlog.com.br>
2023-05-08 13:32:20 -0300

    starting styled components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,10 @@
-import { Text, View } from 'react-native';
+import { Container, Title } from './styles'
 
 export function Groups() {
   return (
-    <View >
-      <Text>Groups</Text>
-    </View>
+    <Container>
+      <Title>Groups</Title>
+    </Container>
   );
 }
 
