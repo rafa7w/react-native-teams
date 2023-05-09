211bf374a9b2460aed217e6e9c2d887f2f3647cb
Rafael William <rafael.fim@hotmail.com>
2023-05-08 21:57:11 -0300

    errors and exceptions

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
+import { Alert } from 'react-native'
 import { Container, Content , Icon} from './styles';
+import { useNavigation } from '@react-navigation/native';
 import { groupCreate } from '@storage/group/groupCreate';
+import { AppError } from '@utils/AppError';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -15,10 +17,20 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.')
+      }
+
       await groupCreate(group)
       navigation.navigate('players', {'group': group})
+
     } catch (error) {
-      console.log(error)
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message)
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.')
+        console.log(error)
+      }
     }
   }
 
