1f08d93ef63cc094833ba29df6faba5ec570544c
Rafael William <rafael.fim@hotmail.com>
2023-05-14 08:43:18 -0300

    storage players

---
+++ src/storage/player/PlayerStorageDTO.ts
@@ -0,0 +1,6 @@
+/* Essa sigla DTO é uma convenção dessas tipagens, objetos que utilizamos para levar de um lugar para outro, por exemplo, da aplicação para uma API, da aplicação para o armazenamento */
+
+export type PlayerStorageDTO = {
+  name: string;
+  team: string;
+}
